401a26a88b30878631627846d25404e81f932293
Briamco <crossinerobriamco@gmail.com>
2026-04-01 09:21:20 -0400

    feat(entities): add Phone property to User entity

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -6,6 +6,7 @@ public class User
   public string FirstName { get; set; } = string.Empty;
   public string LastName { get; set; } = string.Empty;
   public string Email { get; set; } = string.Empty;
+  public string Phone { get; set; } = string.Empty;
   public string Password { get; set; } = string.Empty;
   public bool IsConfirmed { get; set; } = false;
   public string? ConfirmationToken { get; set; }
