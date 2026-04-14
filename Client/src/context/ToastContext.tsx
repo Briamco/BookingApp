ec2640fd94ec1fa48886f995acfc805481f0d900
Briamco <crossinerobriamco@gmail.com>
2026-04-13 21:14:02 -0400

    feat(toast): implement Toast context for managing toast notifications

---
+++ Client/src/context/ToastContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, useCallback, useContext, useState } from "react";
+
+export type ToastType = 'success' | 'error' | 'info' | 'warning';
+
+interface ToastMessage {
+  id: string;
+  type: ToastType;
+  message: string;
+}
+
+interface ToastContextType {
+  addToast: (type: ToastType, message: string) => void;
+}
+
+const ToastContext = createContext<ToastContextType | undefined>(undefined);
+
+export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
+  const [toasts, setToasts] = useState<ToastMessage[]>([]);
+
+  const addToast = useCallback((type: ToastType, message: string) => {
+    const id = Date.now().toString();
+    setToasts((prevToasts) => [...prevToasts, { id, type, message }]);
+    setTimeout(() => {
+      setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+    }, 3000);
+  }, []);
+
+  return (
+    <ToastContext.Provider value={{ addToast }}>
+      {children}
+      <div className="toast toast-top toast-end z-100">
+        {toasts.map((toast) => (
+          <div key={toast.id} className={`alert alert-${toast.type}`}>
+            {toast.message}
+          </div>
+        ))}
+      </div>
+    </ToastContext.Provider>
+  )
+}
+
+export const useToast = (): ToastContextType => {
+  const context = useContext(ToastContext);
+
+  if (context === undefined)
+    throw new Error("useToast must be used within a ToastProvider");
+
+  return context;
+}
